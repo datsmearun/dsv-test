9ec61c700ad8c2f7908d28c4f04d6fd5082d9cc5
Arun <datsmearun@gmail.com>
2024-01-14 14:36:21 +0530

    setup project

---
+++ src/main/java/com/example/imgProcess/ImgProcessApplication.java
@@ -0,0 +1,13 @@
+package com.example.imgProcess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImgProcessApplication.class, args);
+	}
+
+}
